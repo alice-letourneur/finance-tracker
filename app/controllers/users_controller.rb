3e0369d6d30408cd082707a7d31543a0b39530dd
“Alice <aliceletourneur@icloud.com>
2018-03-25 18:48:09 +0100

    user view other users profile and which stocks they are tracking

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,4 +33,9 @@ class UsersController < ApplicationController
     end
     redirect_to my_friends_path
   end
+
+  def show
+    @user = User.find(params[:id])
+    @user_stocks = @user.stocks
+  end
 end
