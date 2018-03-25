ebe443034fc5a3e1711efbf6666bd23af16a57ae
“Alice <aliceletourneur@icloud.com>
2018-03-25 15:59:46 +0100

    create friendship model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+
   def my_portfolio
     @user = current_user
     @user_stocks = current_user.stocks
   end
+
+  def my_friends
+    
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "user/registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'welcome#index'
-get 'my_portfolio', to: 'users#my_portfolio'
-get 'search_stocks', to: 'stocks#search'
-resources :user_stocks, only: [:create, :destroy]
+  root 'welcome#index'
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stocks', to: 'stocks#search'
+  resources :user_stocks, only: [:create, :destroy]
+  get 'my_friends', to: 'users#my_friends'
 end
